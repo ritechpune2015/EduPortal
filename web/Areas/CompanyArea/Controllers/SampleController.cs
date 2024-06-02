b4434e7e49b603497b30e5fd20c4aafc94010268
Achyut Kendre <achyut.kendre@gmail.com>
2024-06-02 15:44:15 +0530

    sample controller added

---
+++ web/Areas/CompanyArea/Controllers/SampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web.Areas.CompanyArea.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
